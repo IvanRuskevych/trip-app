d189689707cf24d8d39ade37bd7d6df5129f868b
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-02-22 01:12:16 +0200

    create weather today panel

--- src/TodayWeather/TodayWeather.jsx
+++ src/TodayWeather/TodayWeather.jsx
@@ -1,28 +1,31 @@
 import PropTypes from "prop-types";
-
-import styles from "../WeekWeather/WeekWeather.module.scss";
+import styles from "./TodayWeather.module.scss";
 import icon from "../assets/weatherIcons/rain.svg";
 
 const TodayWeather = ({selectedTrip, weatherToday}) => {
-    const city = selectedTrip && selectedTrip[0].city
-    // console.log("selectedTrip", selectedTrip && selectedTrip[0].city)
-    // console.log("TodayWeather weatherToday", weatherToday)
+    const city = selectedTrip && selectedTrip[0].city;
+    const weather = weatherToday && weatherToday[0];
+
+    if (!weather) {
+        return null;
+    }
 
     return (
-        <ul className={styles.weatherList}>
-            {weatherToday?.map((item, index) => (
-                <li key={index} className={styles.day}>
-                    <p className={styles.text}>{city}</p>
-                    <img src={icon} alt={item.icon} className={styles.dayIcon}/>
-                    <p className={styles.text}>{`${item.temp}°`}</p>
-                </li>))}
-        </ul>
+        <div className={styles.weatherContainer}>
+            <p className={styles.text}>{weather.weekDay}</p>
+            <div className={styles.wrapper}>
+                <img src={icon} alt={weather.icon} className={styles.icon}/>
+                <p className={styles.textWeather}>{`${weather.temp}`}<sup>°C</sup></p>
+            </div>
+            <p className={styles.text}>{city}</p>
+
+        </div>
     );
 };
 
 TodayWeather.propTypes = {
     weatherToday: PropTypes.array,
     selectedTrip: PropTypes.array,
-}
+};
 
 export default TodayWeather;
