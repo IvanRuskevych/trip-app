29ec7ea7667814e9a871609ef67776a41c17976d
IvanRuskevych <IvanRuskevych@users.noreply.github.com>
2024-02-22 22:05:26 +0000

    deploy: c1740ae960ceda9685871972af1fd50669904f56

--- assets/images/index.js
+++ assets/images/index.js
@@ -3,8 +3,9 @@ import sidney from "./sidney.jpg"
 import tokio from "./tokio.jpg"
 import washington from "./washington.jpg"
 import newyork from "./newyork.jpg"
+import ping from "./ping-icon.png"
 
 
-const images = {london, sidney, tokio, washington, newyork}
+const images = {london, sidney, tokio, washington, newyork, ping}
 
 export default images
